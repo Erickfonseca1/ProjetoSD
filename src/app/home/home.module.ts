32e3a4097258833b8fee6bfdaf7fd04139c355a6
Erick Fonseca <erick@inovenow.com>
2022-10-18 00:45:03 -0300

    Criação do componente 'categories'

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    CategoriesComponent
   ],
   imports: [
     CommonModule

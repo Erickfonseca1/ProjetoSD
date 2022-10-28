3be3109761bea6a99f057198d97348374744537a
Erick Fonseca <erick@inovenow.com>
2022-10-27 23:02:38 -0300

    Criação do componente categories e ajustes na estilização do header

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { MatCardModule} from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    CategoriesComponent
+    CategoriesComponent 
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    CategoriesComponent
   ]
 })
 export class HomeModule { }

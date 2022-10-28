7d6e95824aad2ffab14d5312c9146231d885c153
Walter Filho <walterfilho1000@gmail.com>
2022-10-27 23:58:46 -0300

    fazendo o news

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { NewsComponent } from './news/news.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NewsComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+    HeaderComponent,
+    CategoriesComponent,
+    NewsComponent
   ]
 })
 export class HomeModule { }
